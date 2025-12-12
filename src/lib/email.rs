6dd66395e5f77d4160f8fad9ad68c6411f37618b
Jeff Mitchell <crusty.rustacean@gmail.com>
2025-12-11 19:59:44 -0800

    chore: fix clippy lint regarding unused import in `email.rs`

--- src/lib/email.rs
+++ src/lib/email.rs
@@ -8,7 +8,7 @@
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
-use tracing::{error, info, warn};
+use tracing::{info, warn};
 
 // ============================================================================
 // Core Types
